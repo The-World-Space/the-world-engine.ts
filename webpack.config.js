4d7ca11545cc3d9bd88a684c5e380a7b89aea0d0
noname <48761044+noname0310@users.noreply.github.com>
2022-01-01 20:50:22 +0900

    remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,21 +11,16 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.(ts|js)x?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-typescript",
-            ],
-          },
-        },
+      { 
+        test: /\.tsx?$/,
+        use: [
+          { 
+            loader: 'ts-loader' 
+          }
+        ]
       },
       {
-        test: /\.png/,
+        test: /\.(png|jpg|gif)$/,
         type: 'asset',
       }
     ],
