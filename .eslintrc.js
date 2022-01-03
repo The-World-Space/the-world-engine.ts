9563a4dd2039505e43e83ae60bfb2dfda8578966
noname <48761044+noname0310@users.noreply.github.com>
2022-01-03 18:29:17 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     "rules": {
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
         "no-debugger": "warn",
         "indent": [
             "error",
