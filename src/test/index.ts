5a4f639dafe29d487f1f2b17ad20001b370014de
noname <48761044+noname0310@users.noreply.github.com>
2022-01-03 18:32:26 +0900

    Update index.ts

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,24 +1,5 @@
-import { Vector3 } from "three";
-import { Bootstrapper } from "../engine/bootstrap/Bootstrapper";
-import { SceneBuilder } from "../engine/bootstrap/SceneBuilder";
 import { Game } from "../engine/Game";
-import { Camera } from "../engine/script/render/Camera";
-import { CssSpriteRenderer } from "../engine/script/render/CssSpriteRenderer";
-
-class TestBootstrapper extends Bootstrapper {
-    run(): SceneBuilder {
-        const instantlater = this.engine.instantlater;
-        
-        return this.sceneBuilder
-            .withChild(instantlater.buildGameObject("camera", new Vector3(0, 0, 10))
-                .withComponent(Camera, c => {
-                    c.viewSize = 100;
-                }))
-
-            .withChild(instantlater.buildGameObject("test_object")
-                .withComponent(CssSpriteRenderer));
-    }
-}
+import { TestBootstrapper } from "./TestBootstrapper";
 
 function startTestGame(container: HTMLElement) {
     const game = new Game(container);
