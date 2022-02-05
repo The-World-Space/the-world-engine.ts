e8194637afa8b92d26ea7af13b82ee6a7bd3b912
noname <48761044+noname0310@users.noreply.github.com>
2022-02-05 11:00:35 +0900

    add test components

---
+++ src/test/script/EventTest1.ts
@@ -0,0 +1,28 @@
+import { Component } from "../../engine/hierarchy_object/Component";
+
+export class EventTest1 extends Component {
+    public awake() {
+        console.log("EventTest1.awake()");
+    }
+
+    public onEnable() {
+        console.log("EventTest1.onEnable()");
+        this.destroy();
+    }
+
+    public onDisable() {
+        console.log("EventTest1.onDisable()");
+    }
+
+    public start() {
+        console.log("EventTest1.start()");
+    }
+
+    public update() {
+        console.log("EventTest1.update()");
+    }
+
+    public onDestroy() {
+        console.log("EventTest1.onDestroy()");
+    }
+}
---
+++ src/test/script/EventTest2.ts
@@ -0,0 +1,19 @@
+import { Component } from "../../engine/hierarchy_object/Component";
+
+export class EventTest2 extends Component {
+    public onEnable() {
+        console.log("EventTest2.onEnable()");
+    }
+
+    public start() {
+        console.log("EventTest2.start()");
+    }
+
+    public update() {
+        console.log("EventTest2.update()");
+    }
+
+    public onDestroy() {
+        console.log("EventTest2.onDestroy()");
+    }
+}
