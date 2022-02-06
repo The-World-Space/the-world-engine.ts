ba86d504e1cfff2daa7eeb8d02b01f74a57c815c
noname <48761044+noname0310@users.noreply.github.com>
2022-02-06 23:05:42 +0900

    Update TestBootstrapper.ts

--- src/test/TestBootstrapper.ts
+++ src/test/TestBootstrapper.ts
@@ -1,49 +1,141 @@
 import { Vector2, Vector3 } from "three";
-import { CssHtmlElementRenderer, CssSpriteRenderer, EditorGridRenderer, GameObject, PlayerGridMovementController, PrefabRef } from "..";
+import { Camera, Color, CssHtmlElementRenderer, CssTextRenderer, ParallaxTranslater, TextAlign } from "..";
 import { Bootstrapper } from "../engine/bootstrap/Bootstrapper";
 import { SceneBuilder } from "../engine/bootstrap/SceneBuilder";
-import { Rotator } from "./script/Rotator";
-import { TimeDestroy } from "./script/TimeDestroy";
-import { CameraPrefab } from "./prefab/CameraPrefab";
-import { SansFightRoomPrefab } from "./prefab/SansFightRoomPrefab";
 
 /** @internal */
 export class TestBootstrapper extends Bootstrapper {
     public run(): SceneBuilder {
         const instantiater = this.engine.instantiater;
 
-        const test_object = new PrefabRef<GameObject>();
-
         return this.sceneBuilder
-            .withChild(instantiater.buildGameObject("test_object")
-                .withComponent(TimeDestroy, c => {
-                    c.enabled = false;
-                })
-                .withComponent(CssSpriteRenderer)
-                .withComponent(Rotator, c => c.enabled = false)
-                .withComponent(PlayerGridMovementController)
-                .getGameObject(test_object))
-                
-            .withChild(instantiater.buildGameObject("test_object2", new Vector3(0, 10, 0))
+            .withChild(instantiater.buildGameObject("test_object", new Vector3(0, 10, 0))
                 .withComponent(CssHtmlElementRenderer, c => {
                     c.autoSize = true;
                     const element = document.createElement("div");
-                    element.innerText = "test";
+                    element.innerText = "test1";
                     element.style.backgroundColor = "#F0DB4F";
                     element.style.color = "#323330";
                     c.setElement(element);
                     c.centerOffset = new Vector2(0.5, 0.5);
+                })
+                .withComponent(ParallaxTranslater, c => {
+                    c.offsetX = 0.5;
+                    c.offsetY = 0.5;
+                }))
+            
+            
+            .withChild(instantiater.buildGameObject("test_object2", new Vector3(50, 10, 0))
+                .withComponent(CssHtmlElementRenderer, c => {
+                    c.autoSize = true;
+                    const element = document.createElement("div");
+                    element.innerText = "test2";
+                    element.style.backgroundColor = "#F0DB4F";
+                    element.style.color = "#323330";
+                    c.setElement(element);
+                    c.centerOffset = new Vector2(0.5, 0.5);
+                })
+                .withComponent(ParallaxTranslater, c => {
+                    c.offsetX = 0.5;
+                    c.offsetY = 0.5;
+                }))
+            
+            .withChild(instantiater.buildGameObject("test_object3", new Vector3(100, 10, 0))
+                .withComponent(CssHtmlElementRenderer, c => {
+                    c.autoSize = true;
+                    const element = document.createElement("div");
+                    element.innerText = "test3";
+                    element.style.backgroundColor = "#F0DB4F";
+                    element.style.color = "#323330";
+                    c.setElement(element);
+                    c.centerOffset = new Vector2(0.5, 0.5);
+                })
+                .withComponent(ParallaxTranslater, c => {
+                    c.offsetX = 0.5;
+                    c.offsetY = 0.5;
+                }))
+            
+            .withChild(instantiater.buildGameObject("test_object4", new Vector3(0, -20, 0))
+                .withComponent(CssHtmlElementRenderer, c => {
+                    c.transform.position;
+                    c.autoSize = true;
+                    const element = document.createElement("div");
+                    element.innerText = "test4";
+                    element.style.backgroundColor = "#F0DB4F";
+                    element.style.color = "#323330";
+                    c.setElement(element);
+                    c.centerOffset = new Vector2(0.5, 0.5);
+                })
+                .withComponent(ParallaxTranslater, c => {
+                    c.offsetX = 0.5;
+                    c.offsetY = 0.5;
+                }))
+            
+            .withChild(instantiater.buildGameObject("test_object5", new Vector3(50, -20, 0))
+                .withComponent(CssHtmlElementRenderer, c => {
+                    c.transform.position;
+                    c.autoSize = true;
+                    const element = document.createElement("div");
+                    element.innerText = "test5";
+                    element.style.backgroundColor = "#F0DB4F";
+                    element.style.color = "#323330";
+                    c.setElement(element);
+                    c.centerOffset = new Vector2(0.5, 0.5);
+                })
+                .withComponent(ParallaxTranslater, c => {
+                    c.offsetX = 0.5;
+                    c.offsetY = 0.5;
+                }))
+            
+            .withChild(instantiater.buildGameObject("test_object6", new Vector3(100, -20, 0))
+                .withComponent(CssHtmlElementRenderer, c => {
+                    c.transform.position;
+                    c.autoSize = true;
+                    const element = document.createElement("div");
+                    element.innerText = "test6";
+                    element.style.backgroundColor = "#F0DB4F";
+                    element.style.color = "#323330";
+                    c.setElement(element);
+                    c.centerOffset = new Vector2(0.5, 0.5);
+                })
+                .withComponent(ParallaxTranslater, c => {
+                    c.offsetX = 0.5;
+                    c.offsetY = 0.5;
+                }))
+            
+            .withChild(instantiater.buildGameObject("test_object7", new Vector3(0, -40, 0))
+                //.active(false)
+                .withComponent(CssHtmlElementRenderer, c => {
+                    c.transform.position;
+                    c.autoSize = true;
+                    const element = document.createElement("div");
+                    element.innerText = "test7";
+                    element.style.backgroundColor = "#F0DB4F";
+                    element.style.color = "#323330";
+                    c.setElement(element);
+                    c.centerOffset = new Vector2(0.5, 0.5);
+                })
+                .withComponent(ParallaxTranslater, c => {
+                    c.offsetX = 0.5;
+                    c.offsetY = 0.5;
+                }))
+            
+            .withChild(instantiater.buildGameObject("bug_object", new Vector3(100, -40, 0))
+                //.active(false)
+                .withComponent(CssTextRenderer, c => {
+                    c.transform.position;
+                    c.text = "bug";
+                    c.textAlign = TextAlign.Center;
+                })
+                .withComponent(ParallaxTranslater, c => {
+                    //c.enabled = false;
+                    c.offsetX = 0.5;
+                    c.offsetY = 0.5;
                 }))
 
-            .withChild(instantiater.buildPrefab("sans_fight_room", SansFightRoomPrefab, new Vector3(8, 8, 0))
-                .make())
-            
-            .withChild(instantiater.buildPrefab("camera_controller", CameraPrefab)
-                .withTrackTarget(test_object)
-                .make()
-                .withComponent(EditorGridRenderer, c => {
-                    c.renderWidth = 500;
-                    c.renderHeight = 500;
+            .withChild(instantiater.buildGameObject("camera")
+                .withComponent(Camera, c => {
+                    c.backgroundColor = new Color(0.1, 0.1, 0.1);
                 }))
         ;
     }
