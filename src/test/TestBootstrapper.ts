e87464aca99d0253e3fc6d1c4836cb55e376fe7b
noname <48761044+noname0310@users.noreply.github.com>
2022-02-18 03:03:26 +0900

    add renderer test code

--- src/test/TestBootstrapper.ts
+++ src/test/TestBootstrapper.ts
@@ -1,5 +1,5 @@
-import { Vector3 } from "three";
-import { Camera, Color, CssCollideTilemapChunkRenderer, CssSpriteRenderer, EditorCameraController, EditorGridRenderer, GameObject, PlayerGridMovementController, PointerGridInputListener, PrefabRef } from "..";
+import { Vector2, Vector3 } from "three";
+import { Camera, Color, CssCollideTilemapChunkRenderer, CssHtmlElementRenderer, CssSpriteRenderer, EditorCameraController, EditorGridRenderer, GameObject, PlayerGridMovementController, PointerGridInputListener, PrefabRef } from "..";
 import { Bootstrapper } from "../engine/bootstrap/Bootstrapper";
 import { SceneBuilder } from "../engine/bootstrap/SceneBuilder";
 import { CameraPrefab } from "./prefab/CameraPrefab";
@@ -22,9 +22,46 @@ export class TestBootstrapper extends Bootstrapper {
             )
 
             .withChild(instantiater.buildGameObject("test_object")
-                .active(false)
+                //.active(false)
                 .withComponent(TimeTest, c => c.enabled = false)
-                .withComponent(PointerGridInputListener))
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const element = document.createElement("div");
+                    element.style.backgroundColor = "#dddddd";
+                    element.innerHTML = `
+                        hi! i'm a test object!
+                        <br/>
+                        <br/>
+                        you can add html elements on game objects!
+                        <br/>
+                        <br/>
+                    `;
+                    const button = document.createElement("button");
+                    button.innerText = "click me!";
+                    const counter = document.createElement("span");
+                    counter.innerText = "0";
+                    button.onclick = () => {
+                        counter.innerText = (parseInt(counter.innerText) + 1).toString();
+                    };
+                    element.appendChild(button);
+                    element.appendChild(document.createElement("br"));
+                    element.appendChild(document.createTextNode("count: "));
+                    element.appendChild(counter);
+                    element.appendChild(document.createElement("br"));
+                    const slider = document.createElement("input");
+                    slider.type = "range";
+                    slider.min = "0";
+                    slider.max = "100";
+                    slider.value = "0";
+                    element.appendChild(slider);
+
+                    c.element = element;
+                    c.viewScale = 0.5;
+                    c.autoSize = false;
+                    c.elementWidth = 100;
+                    c.elementHeight = 100;
+                    c.centerOffset = new Vector2(0.5, 0.5);
+                })
+                .withComponent(PointerGridInputListener, c => c.enabled = false))
 
             .withChild(instantiater.buildGameObject("track_object")
                 //.active(false)
