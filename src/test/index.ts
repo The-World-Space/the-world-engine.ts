c7d9c6ee888d9d0096dc25994441d729777a6ae0
noname <48761044+noname0310@users.noreply.github.com>
2022-02-26 15:01:21 +0900

    Update index.ts

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,6 +1,5 @@
 import { Game } from "../engine/Game";
 import { TestBootstrapper } from "./asset/TestBootstrapper";
-//import { mutIteratableCollectionTest2 } from "./../engine/collection/CollectionTest";
 
 function startTestGame(container: HTMLElement) {
     const game = new Game(container);
@@ -8,6 +7,4 @@ function startTestGame(container: HTMLElement) {
     game.inputHandler.startHandleEvents();
 }
 
-//mutIteratableCollectionTest1();
-//mutIteratableCollectionTest2();
 startTestGame(document.body);
