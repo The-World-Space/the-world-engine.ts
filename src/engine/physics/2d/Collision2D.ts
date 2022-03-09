7f49759d016814f661f41f9738b4e4d0f87557e2
noname <48761044+noname0310@users.noreply.github.com>
2022-03-09 18:09:40 +0900

    Update Collision2D.ts

--- src/engine/physics/2d/Collision2D.ts
+++ src/engine/physics/2d/Collision2D.ts
@@ -1,3 +1,36 @@
-export class Collision2D {
+//import * as b2 from "../../../box2d.ts/build/index";
+import { Vector2 } from "three";
+import { Collider2D } from "../../script/physics2d/collider/Collider2D";
+import { RigidBody2D } from "../../script/physics2d/RigidBody2D";
 
+export class Collision2D {
+    //this
+    public collider: Collider2D;
+    public rigidbody: RigidBody2D|null;
+
+    //other
+    public otherCollider: Collider2D;
+    public otherRigidbody: RigidBody2D|null;
+
+    public contactCount: number;
+    public contacts: unknown;
+    public enabled: boolean;
+    public relativeVelocity: Vector2;
+
+    public constructor(
+        collider: Collider2D,
+        rigidbody: RigidBody2D|null,
+        otherCollider: Collider2D,
+        otherRigidbody: RigidBody2D|null,
+    ) {
+        this.collider = collider;
+        this.rigidbody = rigidbody;
+        this.otherCollider = otherCollider;
+        this.otherRigidbody = otherRigidbody;
+
+        this.contactCount = 0;
+        this.contacts = null;
+        this.enabled = true;
+        this.relativeVelocity = new Vector2();
+    }
 }
