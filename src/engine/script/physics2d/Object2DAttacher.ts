0cff7bc6704b2fea027c303a9c61ba5a1c30c22b
noname <48761044+noname0310@users.noreply.github.com>
2022-03-16 15:25:28 +0900

    Update Object2DAttacher.ts

--- src/engine/script/physics2d/Object2DAttacher.ts
+++ src/engine/script/physics2d/Object2DAttacher.ts
@@ -9,13 +9,13 @@ export class Object2DAttacher extends Component {
         this._target = value;
         value.transform.localPosition.x = this.transform.position.x; //use local value for performance reason
         value.transform.localPosition.y = this.transform.position.y;
-        value.transform.localEulerAngles.z = this.transform.localEulerAngles.z;
+        value.transform.localEulerAngles.z = this.transform.eulerAngles.z;
     }
 
     public onWorldMatrixUpdated(): void {
         const targetTransform = this._target!.transform;
         targetTransform.localPosition.x = this.transform.position.x; //use local value for performance reason
         targetTransform.localPosition.y = this.transform.position.y;
-        targetTransform.localEulerAngles.z = this.transform.localEulerAngles.z;
+        targetTransform.localEulerAngles.z = this.transform.eulerAngles.z;
     }
 }
