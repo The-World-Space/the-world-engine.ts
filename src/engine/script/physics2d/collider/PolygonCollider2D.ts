1a2989423de3263928a3d6ff7edccf36020369ba
noname <48761044+noname0310@users.noreply.github.com>
2022-03-17 16:30:57 +0900

    Create PolygonCollider2D.ts

---
+++ src/engine/script/physics2d/collider/PolygonCollider2D.ts
@@ -0,0 +1,79 @@
+import type { Shape } from "../../../../box2d.ts/build/index";
+import { PolygonShape } from "../../../../box2d.ts/build/index";
+import { Vector2, Vector3 } from "three/src/Three";
+import { Collider2D } from "./Collider2D";
+import type { GameObject } from "../../../hierarchy_object/GameObject";
+import type { ReadonlyVector2 } from "../../../math/ReadonlyVector2";
+import { getOrCreatePhysicsDebugRenderObject } from "../PhysicsDebugRender";
+import { Object2DAttacher } from "../Object2DAttacher";
+import { Css2DPolygonRenderer } from "../../render/Css2DPolygonRenderer";
+import { PrefabRef } from "../../../hierarchy_object/PrefabRef";
+import { Color } from "../../../render/Color";
+
+export class PolygonCollider2D extends Collider2D {
+    private _points: Vector2[] = [];
+    private _debugDraw = true;
+    private _debugObject: GameObject|null = null;
+    private _debugRenderer: Css2DPolygonRenderer|null = null;
+
+    public override onEnable(): void {
+        super.onEnable();
+        if (this._debugDraw) {
+            const objectAttacher = this.gameObject.addComponent(Object2DAttacher);
+
+            const physicsDebugRenderObject = getOrCreatePhysicsDebugRenderObject(this.engine);
+            const debugRenderer = new PrefabRef<Css2DPolygonRenderer>();
+            this._debugObject = physicsDebugRenderObject.addChildFromBuilder(
+                this.engine.instantiater.buildGameObject(this.gameObject.name + "_debug_polygon")
+                    .withChild(this.engine.instantiater.buildGameObject("debug_polygon", new Vector3(this.offset.x, this.offset.y, 200))
+                        .withComponent(Css2DPolygonRenderer, c => {
+                            c.points = this._points;
+                            c.viewScale = 0.01;
+                            c.color = new Color(0, 0, 0, 0);
+                            c.borderWidth = 2;
+                            c.borderColor = new Color(1, 1, 0, 0.3);
+                        })
+                        .getComponent(Css2DPolygonRenderer, debugRenderer)));
+            
+            this._debugRenderer = debugRenderer.ref;
+            objectAttacher!.target = this._debugObject;
+        }
+    }
+
+    public override onDisable(): void {
+        super.onDisable();
+        if (this._debugObject) {
+            this._debugObject.destroy();
+            this._debugObject = null;
+        }
+    }
+
+    protected override createShape(): Shape {
+        const shape = new PolygonShape();
+        shape.Set(this._points);
+        return shape;
+    }
+
+    public get points(): readonly ReadonlyVector2[] {
+        return this._points;
+    }
+
+    public set points(value: readonly ReadonlyVector2[]) {
+        this._points.length = 0;
+        for (let i = 0; i < value.length; i++) {
+            this._points.push(value[i].clone());
+        }
+        this.updateFixture();
+        if (this._debugRenderer) {
+            this._debugRenderer.points = this._points;
+        }
+    }
+
+    public get debugDraw(): boolean {
+        return this._debugDraw;
+    }
+
+    public set debugDraw(value: boolean) {
+        this._debugDraw = value;
+    }
+}
