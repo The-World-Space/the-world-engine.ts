5096ded3bf084e67a9f9aff7f191625d855d796e
noname <48761044+noname0310@users.noreply.github.com>
2022-03-17 17:35:23 +0900

    Update PhysicsController.ts

--- src/test/asset/script/PhysicsController.ts
+++ src/test/asset/script/PhysicsController.ts
@@ -9,26 +9,24 @@ export class PhysicsController extends Component {
     private _rigidbody: RigidBody2D|null = null;
 
     public awake(): void {
-        this.engine.input.addOnKeyDownEventListener(this.onKeyDown);
         this._rigidbody = this.gameObject.getComponent(RigidBody2D);
-    }
-
-    public onDestroy(): void {
-        this.engine.input.removeOnKeyDownEventListener(this.onKeyDown);
     }
 
     private _tempVector2 = new Vector2();
 
-    private onKeyDown = (event: KeyboardEvent): void => {
-        if (!this._rigidbody) return;
-        if (event.key === "ArrowUp") {
-            this._rigidbody.addForce(this._tempVector2.set(0, this.force));
-        } else if (event.key === "ArrowDown") {
-            this._rigidbody.addForce(this._tempVector2.set(0, -this.force));
-        } else if (event.key === "ArrowLeft") {
-            this._rigidbody.addForce(this._tempVector2.set(-this.force, 0));
-        } else if (event.key === "ArrowRight") {
-            this._rigidbody.addForce(this._tempVector2.set(this.force, 0));
+    public update(): void {
+        const inputMap = this.engine.input.map;
+        if (inputMap.get("ArrowUp")) {
+            this._rigidbody!.addForce(this._tempVector2.set(0, this.force));
         }
-    };
+        if (inputMap.get("ArrowDown")) {
+            this._rigidbody!.addForce(this._tempVector2.set(0, -this.force));
+        }
+        if (inputMap.get("ArrowLeft")) {
+            this._rigidbody!.addForce(this._tempVector2.set(-this.force, 0));
+        }
+        if (inputMap.get("ArrowRight")) {
+            this._rigidbody!.addForce(this._tempVector2.set(this.force, 0));
+        }
+    }
 }
