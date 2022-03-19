15b6bb5ae29b21c9991b21d4a9d72b96a274a76c
noname <48761044+noname0310@users.noreply.github.com>
2022-03-19 12:10:48 +0900

    Update TestBootstrapper.ts

--- src/test/asset/TestBootstrapper.ts
+++ src/test/asset/TestBootstrapper.ts
@@ -1,7 +1,6 @@
 import { Quaternion, Vector2, Vector3 } from "three/src/Three";
 import { Bootstrapper } from "../../engine/bootstrap/Bootstrapper";
 import { SceneBuilder } from "../../engine/bootstrap/SceneBuilder";
-import { CameraPrefab } from "./prefab/CameraPrefab";
 import { SansFightRoomPrefab } from "./prefab/SansFightRoomPrefab";
 import { FpsCounter } from "./script/FpsCounter";
 import { TestLayer } from "./TestLayer";
@@ -29,6 +28,7 @@ import { CircleCollider2D } from "../../engine/script/physics2d/collider/CircleC
 import { PolygonCollider2D } from "../../engine/script/physics2d/collider/PolygonCollider2D";
 import { EdgeCollider2D } from "../../engine/script/physics2d/collider/EdgeCollider2D";
 import { RenderTestPrefab } from "./prefab/RenderTestPrefab";
+import { ZaxisSorter } from "../../engine/script/render/ZaxisSorter";
 
 /** @internal */
 export class TestBootstrapper extends Bootstrapper {
@@ -163,74 +163,47 @@ export class TestBootstrapper extends Bootstrapper {
                     c.imageFlipY = true;
                     c.imageWidth = 1;
                     c.imageHeight = 2;
-                    c.centerOffset = new Vector2(0, 0);
+                    c.centerOffset = new Vector2(0, 0.4);
                     c.renderMode = CssSpriteAtlasRenderMode.ObjectFit;
-                    //settimeout loop
-                    setTimeout(() => {
-                        c.imageIndex = 1;
-                        setTimeout(() => {
-                            c.imageIndex = 2;
-                            setTimeout(() => {
-                                c.imageIndex = 3;
-                                setTimeout(() => {
-                                    c.imageIndex = 4;
-                                    setTimeout(() => {
-                                        c.imageIndex = 5;
-                                    }, 1000);
-                                }, 1000);
-                            }, 1000);
-                        }, 1000);
-                    }, 1000);
                 })
                 .withComponent(PlayerGridMovementController, c => {
                     c.setGridInfoFromCollideMap(gridMap.ref!);
                     c.addCollideMap(gridMap.ref!);
                 })
+                .withComponent(ZaxisSorter)
                 .getGameObject(trackObject))
 
             .withChild(instantiater.buildPrefab("render_test", RenderTestPrefab, new Vector3(0, -25, 0)).make())
             
-            .withChild(instantiater.buildGameObject("camera_parent")
-                .withChild(instantiater.buildGameObject("editor_camera", new Vector3(0, 0, 80))
-                    //.active(false)
-                    .withComponent(Camera, c => {
-                        c.viewSize = 10;
-                        c.backgroundColor = new Color(0, 0, 0);
-                        c.cameraType = CameraType.Orthographic;
-                    })
-                    .withComponent(EditorCameraController, c => {
-                        c.maxViewSize = 40;
-                    })
-                    .withComponent(EditorGridRenderer, c => {
-                        c.enabled = false;
-                        c.renderWidth = 100;
-                        c.renderHeight = 100;
-                    })
+            .withChild(instantiater.buildGameObject("editor_camera", new Vector3(0, 0, 80))
+                //.active(false)
+                .withComponent(Camera, c => {
+                    c.viewSize = 10;
+                    c.backgroundColor = new Color(0, 0, 0);
+                    c.cameraType = CameraType.Orthographic;
+                })
+                .withComponent(EditorCameraController, c => {
+                    c.maxViewSize = 40;
+                    c.mouseMoveButton = 0;
+                })
+                .withComponent(EditorGridRenderer, c => {
+                    c.enabled = false;
+                    c.renderWidth = 100;
+                    c.renderHeight = 100;
+                })
+                .withComponent(CssTextRenderer, c => {
+                    c.pointerEvents = false;
+                    c.text = "e : spawn object  d : despawn object";
+                    c.textWidth = 18;
+                    c.centerOffset = new Vector2(0, 3);
+                })
+                .withChild(instantiater.buildGameObject("fps_counter")
                     .withComponent(CssTextRenderer, c => {
-                        c.pointerEvents = false;
-                        c.text = "e : spawn object  d : despawn object";
-                        c.textWidth = 18;
-                        c.centerOffset = new Vector2(0, 3);
+                        c.autoSize = true;
+                        c.centerOffset = new Vector2(-1.8, 1.8);
+                        c.textColor = new Color(1, 1, 1);
                     })
-                    .withChild(instantiater.buildGameObject("fps_counter")
-                        .withComponent(CssTextRenderer, c => {
-                            c.autoSize = true;
-                            c.centerOffset = new Vector2(-1.8, 1.8);
-                            c.textColor = new Color(1, 1, 1);
-                        })
-                        .withComponent(FpsCounter)))
-
-                .withChild(instantiater.buildPrefab("track_camera", CameraPrefab)
-                    .withTrackTarget(trackObject)
-                    .withBackgroundColor(new PrefabRef(new Color(0, 0, 0)))
-                    .withViewSize(new PrefabRef(10))
-                    .make()
-                    .active(false)
-                    .withComponent(EditorGridRenderer, c => {
-                        //c.enabled = false;
-                        c.renderWidth = 100;
-                        c.renderHeight = 100;
-                    })))
+                    .withComponent(FpsCounter)))
         ;
     }
 }
