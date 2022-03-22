70225a39ced7e1778487c425b6d4a94dfbeba14d
noname <48761044+noname0310@users.noreply.github.com>
2022-03-22 11:31:44 +0900

    change edge renderer to line renderer

--- src/test/asset/script/RaycastTest.ts
+++ src/test/asset/script/RaycastTest.ts
@@ -3,25 +3,26 @@ import { Component } from "../../../engine/hierarchy_object/Component";
 import { WritableVector2 } from "../../../engine/math/WritableVector2";
 import { RaycastHit2D } from "../../../engine/physics/2d/RaycastHit2D";
 import { Color } from "../../../engine/render/Color";
-import { Css2DEdgeRenderer } from "../../../engine/script/render/Css2DEdgeRenderer";
+import { Css2DLineRenderer } from "../../../engine/script/render/Css2DLineRenderer";
 //import { Css2DPolygonRenderer } from "../../../engine/script/render/Css2DPolygonRenderer";
 
 export class RaycastTest extends Component {
-    private _edgeRenderer: Css2DEdgeRenderer|null = null;
+    private _lineRenderer: Css2DLineRenderer|null = null;
     //private _polygonRenderer: Css2DPolygonRenderer|null = null;
     private _degrees = 0;
     private readonly _directionVector = new Vector2();
     private readonly _positionVector2 = new Vector2();
     private readonly _raycastHit = new RaycastHit2D();
-    private readonly _points: [Vector2, Vector2] = [ new Vector2(), new Vector2() ];
+    private readonly _rayPoint = new Vector2();
     // private readonly _polygonPoints: Vector2[] = [];
     // private _frameCount = 0;
 
     public awake(): void {
-        this._edgeRenderer = this.gameObject.addComponent(Css2DEdgeRenderer)!;
-        this._edgeRenderer.edgeWidth = 2;
-        this._edgeRenderer.edgeColor = new Color(1, 1, 1);
-        this._edgeRenderer.viewScale = 0.01;
+        this._lineRenderer = this.gameObject.addComponent(Css2DLineRenderer)!;
+        this._lineRenderer.lineWidth = 0.02;
+        this._lineRenderer.lineColor = new Color(1, 1, 1);
+        this._lineRenderer.viewScale = 0.01;
+        this._lineRenderer.point1 = new Vector2();
 
         // this._polygonRenderer = this.gameObject.addComponent(Css2DPolygonRenderer)!;
         // this._polygonRenderer.viewScale = 0.01;
@@ -38,8 +39,8 @@ export class RaycastTest extends Component {
             this.gameObject.transform.position.y
         );
         const result = this.engine.physics.raycastOne(position, direction, this._raycastHit);
-        (this._points[1] as WritableVector2).copy(result?.point ?? direction.multiplyScalar(100)).sub(position);
-        this._edgeRenderer!.points = this._points;
+        (this._rayPoint as WritableVector2).copy(result?.point ?? direction.multiplyScalar(100)).sub(position);
+        this._lineRenderer!.point2 = this._rayPoint;
 
         // if (this._frameCount++ % 4 === 0) {
         //     this._polygonPoints.push(this._endPoint.clone());
