97f5040a4f0202cfb76537bc4f735a606f788ae0
noname <48761044+noname0310@users.noreply.github.com>
2022-03-23 21:03:51 +0900

    doc update

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -6,112 +6,105 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+    title: 'the-world-engine',
+    tagline: 'three.js based, unity like game engine for browser',
+    url: 'https://the-world-space.github.io/',
+    baseUrl: process.env.NODE_ENV === 'production' ? '/the-world-engine/build/' : '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'noname0310',
+    projectName: 'the-world-engine.ts',
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    editUrl: 'https://github.com/The-World-Space/the-world-engine.ts/tree/main/docs/docusaurus/',
+                },
+                // blog: {
+                //   showReadingTime: true,
+                //   editUrl:
+                //     ''https://github.com/The-World-Space/the-world-engine.ts/tree/main/docs/docusaurus/',
+                // },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+        ({
+        navbar: {
+            title: 'the-world-engine',
+            logo: {
+                alt: 'the-world logo',
+                src: 'img/logo.svg',
+            },
+            items: [{
+                    type: 'doc',
+                    docId: 'intro',
+                    position: 'left',
+                    label: 'Tutorial',
+                },
+                // { to: '/blog', label: 'Blog', position: 'left' },
+                {
+                    href: 'https://github.com/The-World-Space/the-world-engine.ts',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
         },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
+        footer: {
+            style: 'dark',
+            links: [
+                // {
+                //     title: 'Docs',
+                //     items: [{
+                //         label: 'Tutorial',
+                //         to: '/docs/intro',
+                //     }, ],
+                // },
+                // {
+                //     title: 'Community',
+                //     items: [{
+                //             label: 'Stack Overflow',
+                //             href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                //         },
+                //         {
+                //             label: 'Discord',
+                //             href: 'https://discordapp.com/invite/docusaurus',
+                //         },
+                //         {
+                //             label: 'Twitter',
+                //             href: 'https://twitter.com/docusaurus',
+                //         },
+                //     ],
+                // },
+                // {
+                //     title: 'More',
+                //     items: [{
+                //             label: 'Blog',
+                //             to: '/blog',
+                //         },
+                //         {
+                //             label: 'GitHub',
+                //             href: 'https://github.com/facebook/docusaurus',
+                //         },
+                //     ],
+                // },
             ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
+            copyright: `Copyright © ${new Date().getFullYear()} the-world-engine, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+        },
     }),
 };
 
