4e8f42a1430e01e8d1d7cf20ca10b13d137e26dd
noname <48761044+noname0310@users.noreply.github.com>
2022-05-06 17:10:32 +0900

    fix lint

--- src/engine/type/DeepReadonly.ts
+++ src/engine/type/DeepReadonly.ts
@@ -1,6 +1,7 @@
 export type DeepReadonly<T> =
     T extends (infer R)[] ? DeepReadonlyArray<R> :
-    T extends (...args: any[]) => any ? T :
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    T extends Function ? T :
     T extends object ? DeepReadonlyObject<T> :
     T;
 
