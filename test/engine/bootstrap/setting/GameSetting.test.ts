5de2381fc7a7b12969fcb9ab9a6334010800d2cf
noname <48761044+noname0310@users.noreply.github.com>
2022-05-06 23:14:21 +0900

    Update GameSetting.test.ts

--- test/engine/bootstrap/setting/GameSetting.test.ts
+++ test/engine/bootstrap/setting/GameSetting.test.ts
@@ -1,7 +1,18 @@
-import { GameSetting } from "src/engine/bootstrap/setting/GameSetting";
+import { GameSetting } from "@src/engine/bootstrap/setting/GameSetting";
+import { PhysicsSetting } from "@src/engine/bootstrap/setting/PhysicsSetting";
+import { RenderSetting } from "@src/engine/bootstrap/setting/RenderSetting";
+import { Vector2 } from "three";
 
 describe("GameSetting Test", () => {
-    test("GameSetting.createDefaultObject()", () => {
+    it("GameSetting.constructor", () => {
+        const settingObject = GameSetting.createDefaultObject();
+        const setting = new GameSetting(settingObject);
+
+        expect(setting).toBeDefined();
+        expect(setting.constructor).toEqual(GameSetting);
+    });
+
+    it("GameSetting.createDefaultObject()", () => {
         const settingObject = GameSetting.createDefaultObject();
         
         expect(settingObject).toEqual({
@@ -10,5 +21,33 @@ describe("GameSetting Test", () => {
             },
             physics: { }
         });
+    });
+
+    it("GameSetting.render", () => {
+        const settingObject = GameSetting.createDefaultObject();
+        const setting = new GameSetting(settingObject);
+
+        expect(setting.render).toBeDefined();
+        expect(setting.render.constructor).toEqual(RenderSetting);
+    });
+
+    it("GameSetting.physics", () => {
+        const settingObject = GameSetting.createDefaultObject();
+        const setting = new GameSetting(settingObject);
+
+        expect(setting.physics).toBeDefined();
+        expect(setting.physics.constructor).toEqual(PhysicsSetting);
+    });
+
+    it("GameSetting.make()", () => {
+        const settingObject = GameSetting.createDefaultObject();
+        const setting = new GameSetting(settingObject);
+
+        setting.physics
+            .gravity(new Vector2(1, 2));
+
+        const readonlySetting = setting.make();
+
+        expect(readonlySetting.physics.gravity).toEqual(new Vector2(1, 2));
     });
 });
