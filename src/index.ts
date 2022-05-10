b32821819d2df7c191ba803cbfb81bf672121dee
noname <48761044+noname0310@users.noreply.github.com>
2022-05-10 19:31:24 +0900

    remove unnecessary as

--- src/index.ts
+++ src/index.ts
@@ -73,7 +73,7 @@ export type { IReadonlyGameState } from "./engine/GameState";
 export { GameStateKind } from "./engine/GameState";
 export { Instantiater } from "./engine/Instantiater";
 
-export { GlobalConfig as GlobalConfig } from "./GlobalConfig";
+export { GlobalConfig } from "./GlobalConfig";
 
 
 //engine_internal_script
