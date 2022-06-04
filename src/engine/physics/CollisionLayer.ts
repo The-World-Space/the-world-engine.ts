6b2f86be3a6bb21cb4947d759c94cf3bf99483a7
noname <48761044+noname0310@users.noreply.github.com>
2022-06-04 19:50:34 +0900

    fix: compilation error due to parameter name

--- src/engine/physics/CollisionLayer.ts
+++ src/engine/physics/CollisionLayer.ts
@@ -5,7 +5,7 @@ export type CollisionLayer<l extends number = 32, acc extends string[] = [Collis
         ? acc 
         : acc | CollisionLayer<l, [...acc, string]>;
 
-export type CollisionLayerParm<T extends CollisionLayer, acc extends boolean[] = [], out extends string = CollisionLayerConstType.DefaultLayerName> =
+export type CollisionLayerParm<T extends CollisionLayer, acc extends boolean[] = [], result extends string = CollisionLayerConstType.DefaultLayerName> =
     acc["length"] extends T["length"] 
-        ? out
-        : out | CollisionLayerParm<T, [...acc, true], T[acc["length"]]>;
+        ? result
+        : result | CollisionLayerParm<T, [...acc, true], T[acc["length"]]>;
