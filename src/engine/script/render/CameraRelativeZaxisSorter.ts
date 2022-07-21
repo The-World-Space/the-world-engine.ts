b0b0687874e520b55d87fb35d86938d2af003a2e
noname <48761044+noname0310@users.noreply.github.com>
2022-07-21 11:03:35 +0900

    docs: add comment

--- src/engine/script/render/CameraRelativeZaxisSorter.ts
+++ src/engine/script/render/CameraRelativeZaxisSorter.ts
@@ -1,5 +1,16 @@
 import { ZaxisSortable } from "./ZaxisSortable";
 
+/**
+ * Determine the z-value of the object based on the z-position of the camera
+ * 
+ * This component is mainly used to create top-down 2D games.
+ * 
+ * You can use this component on objects like tilemaps that must always be a certain distance away from the camera
+ * 
+ * 
+ * This component also matches the z-index value of the css renderer to the z value.
+ * This feature is simply for fixing bugs in a Chromium-based browser, and the bug has been fixed in the latest patch so this feature will soon be removed.
+ */
 export class CameraRelativeZaxisSorter extends ZaxisSortable {
     public override readonly disallowMultipleComponent: boolean = true;
 
@@ -17,10 +28,16 @@ export class CameraRelativeZaxisSorter extends ZaxisSortable {
         });
     }
 
+    /**
+     * The offset to be added to the z-position of the camera (default: -100)
+     */
     public get offset(): number {
         return this._offset;
     }
 
+    /**
+     * The offset to be added to the z-position of the camera (default: -100)
+     */
     public set offset(value: number) {
         this._offset = value;
     }
