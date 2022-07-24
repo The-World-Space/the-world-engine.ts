871ed8ae9c9f8779ce7324e4aa7a922dbf6c1a7f
noname <48761044+noname0310@users.noreply.github.com>
2022-07-24 13:32:09 +0900

    docs: create 1-8

---
+++ docs/docusaurus/docs/1-getting-started/8-add-object-dynamically/Bootstrapper.ts
@@ -0,0 +1,76 @@
+import { 
+    Bootstrapper as BaseBootstrapper,
+    Camera,
+    SceneBuilder
+} from "the-world-engine";
+import { RandomSpawner1, RandomSpawner2, RandomSpawner3, RandomSpawner4, RandomSpawner5 } from "./RandomSpawner";
+
+export class Bootstrapper1 extends BaseBootstrapper {
+    public run(): SceneBuilder {
+        const instantiater = this.instantiater;
+
+        return this.sceneBuilder
+            .withChild(instantiater.buildGameObject("camera")
+                .withComponent(Camera))
+
+            .withChild(instantiater.buildGameObject("spawner")
+                .withComponent(RandomSpawner1))
+        ;
+    }
+}
+
+export class Bootstrapper2 extends BaseBootstrapper {
+    public run(): SceneBuilder {
+        const instantiater = this.instantiater;
+
+        return this.sceneBuilder
+            .withChild(instantiater.buildGameObject("camera")
+                .withComponent(Camera))
+
+            .withChild(instantiater.buildGameObject("spawner")
+                .withComponent(RandomSpawner2))
+        ;
+    }
+}
+
+export class Bootstrapper3 extends BaseBootstrapper {
+    public run(): SceneBuilder {
+        const instantiater = this.instantiater;
+
+        return this.sceneBuilder
+            .withChild(instantiater.buildGameObject("camera")
+                .withComponent(Camera))
+
+            .withChild(instantiater.buildGameObject("spawner")
+                .withComponent(RandomSpawner3))
+        ;
+    }
+}
+
+export class Bootstrapper4 extends BaseBootstrapper {
+    public run(): SceneBuilder {
+        const instantiater = this.instantiater;
+
+        return this.sceneBuilder
+            .withChild(instantiater.buildGameObject("camera")
+                .withComponent(Camera))
+
+            .withChild(instantiater.buildGameObject("spawner")
+                .withComponent(RandomSpawner4))
+        ;
+    }
+}
+
+export class Bootstrapper5 extends BaseBootstrapper {
+    public run(): SceneBuilder {
+        const instantiater = this.instantiater;
+
+        return this.sceneBuilder
+            .withChild(instantiater.buildGameObject("camera")
+                .withComponent(Camera))
+
+            .withChild(instantiater.buildGameObject("spawner")
+                .withComponent(RandomSpawner5))
+        ;
+    }
+}
---
+++ docs/docusaurus/docs/1-getting-started/8-add-object-dynamically/RandomSpawner.ts
@@ -0,0 +1,293 @@
+import { Color, Component, CssHtmlElementRenderer, CssTextRenderer, GameObject, PrefabRef, TextAlign } from "the-world-engine";
+import { Vector3 } from "three/src/Three";
+
+export class RandomSpawner1 extends Component {
+    private readonly _range: number = 6;
+
+    public spawnAtScene(): void {
+        const instantiater = this.engine.instantiater;
+        
+        const x = Math.random() * this._range - this._range / 2;
+        const y = Math.random() * this._range - this._range / 2;
+        const position = new Vector3(x, y, 0);
+
+        this.engine.scene.addChildFromBuilder(
+            instantiater.buildGameObject("spawned-game-object", position)
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const div = document.createElement("div");
+                    div.style.backgroundColor = "black";
+                    div.style.color = "white";
+                    div.style.padding = "10px";
+                    div.textContent = "Spawned At Scene";
+
+                    c.element = div;
+                    c.autoSize = true;
+                    c.viewScale = 0.02;
+                }));
+    }
+
+    public awake(): void {
+        for (let i = 0; i < 10; i++) {
+            this.spawnAtScene();
+        }
+    }
+}
+
+export class RandomSpawner2 extends Component {
+    private readonly _range: number = 6;
+
+    public spawnAtScene(): void {
+        const instantiater = this.engine.instantiater;
+        
+        const x = Math.random() * this._range - this._range / 2;
+        const y = Math.random() * this._range - this._range / 2;
+        const position = new Vector3(x, y, 0);
+
+        this.engine.scene.addChildFromBuilder(
+            instantiater.buildGameObject("spawned-game-object", position)
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const div = document.createElement("div");
+                    div.style.backgroundColor = "black";
+                    div.style.color = "white";
+                    div.style.padding = "10px";
+                    div.textContent = "Spawned At Scene";
+
+                    c.element = div;
+                    c.autoSize = true;
+                    c.viewScale = 0.02;
+                }));
+    }
+
+    public spawnAtGameObject(): void {
+        const instantiater = this.engine.instantiater;
+        
+        const x = Math.random() * this._range - this._range / 2;
+        const y = Math.random() * this._range - this._range / 2;
+        const position = new Vector3(x, y, 0);
+
+        this.gameObject.addChildFromBuilder(
+            instantiater.buildGameObject("spawned-game-object", position)
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const div = document.createElement("div");
+                    div.style.backgroundColor = "black";
+                    div.style.color = "white";
+                    div.style.padding = "10px";
+                    div.textContent = "Spawned At GameObject";
+
+                    c.element = div;
+                    c.autoSize = true;
+                    c.viewScale = 0.02;
+                }));
+    }
+
+    public awake(): void {
+        for (let i = 0; i < 10; i++) {
+            this.spawnAtGameObject();
+        }
+    }
+}
+
+export class RandomSpawner3 extends Component {
+    private readonly _range: number = 6;
+
+    public spawnAtScene(): void {
+        const instantiater = this.engine.instantiater;
+        
+        const x = Math.random() * this._range - this._range / 2;
+        const y = Math.random() * this._range - this._range / 2;
+        const position = new Vector3(x, y, 0);
+
+        this.engine.scene.addChildFromBuilder(
+            instantiater.buildGameObject("spawned-game-object", position)
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const div = document.createElement("div");
+                    div.style.backgroundColor = "black";
+                    div.style.color = "white";
+                    div.style.padding = "10px";
+                    div.textContent = "Spawned At Scene";
+
+                    c.element = div;
+                    c.autoSize = true;
+                    c.viewScale = 0.02;
+                }));
+    }
+
+    public spawnAtGameObject(): void {
+        const instantiater = this.engine.instantiater;
+        
+        const x = Math.random() * this._range - this._range / 2;
+        const y = Math.random() * this._range - this._range / 2;
+        const position = new Vector3(x, y, 0);
+
+        this.gameObject.addChildFromBuilder(
+            instantiater.buildGameObject("spawned-game-object", position)
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const div = document.createElement("div");
+                    div.style.backgroundColor = "black";
+                    div.style.color = "white";
+                    div.style.padding = "10px";
+                    div.textContent = "Spawned At GameObject";
+
+                    c.element = div;
+                    c.autoSize = true;
+                    c.viewScale = 0.02;
+                }));
+    }
+
+    public awake(): void {
+        const textRenderer = this.gameObject.addComponent(CssTextRenderer)!;
+        textRenderer.text = "Spawner";
+        textRenderer.textColor = new Color(1, 0, 0);
+        textRenderer.textAlign = TextAlign.Center;
+        textRenderer.textWidth = 5;
+
+        for (let i = 0; i < 10; i++) {
+            this.spawnAtGameObject();
+        }
+    }
+}
+
+export class RandomSpawner4 extends Component {
+    private readonly _range: number = 6;
+    private readonly _spawnedObjects: GameObject[] = [];
+
+    public spawnAtGameObject(): void {
+        const instantiater = this.engine.instantiater;
+        
+        const x = Math.random() * this._range - this._range / 2;
+        const y = Math.random() * this._range - this._range / 2;
+        const position = new Vector3(x, y, 0);
+
+        const spawnedObject = this.gameObject.addChildFromBuilder(
+            instantiater.buildGameObject("spawned-game-object", position)
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const div = document.createElement("div");
+                    div.style.backgroundColor = "black";
+                    div.style.color = "white";
+                    div.style.padding = "10px";
+                    div.textContent = "Spawned At GameObject";
+
+                    c.element = div;
+                    c.autoSize = true;
+                    c.viewScale = 0.02;
+                }));
+
+        this._spawnedObjects.push(spawnedObject);
+    }
+
+    public destroyAllSpawnedObjects(): void {
+        for (const spawnedObject of this._spawnedObjects) {
+            spawnedObject.destroy();
+        }
+    }
+
+    public awake(): void {
+        const textRenderer = this.gameObject.addComponent(CssTextRenderer)!;
+        textRenderer.text = "Spawner";
+        textRenderer.textColor = new Color(1, 0, 0);
+        textRenderer.textAlign = TextAlign.Center;
+        textRenderer.textWidth = 5;
+
+        this.engine.scene.addChildFromBuilder(
+            this.engine.instantiater.buildGameObject("remove-all-spawned-objects-button", new Vector3(0, 0, 5))
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const button = document.createElement("button");
+                    button.textContent = "Remove All Spawned Objects";
+                    button.style.backgroundColor = "#990000";
+                    button.style.color = "white";
+                    button.style.padding = "10px";
+                    button.onclick = () => {
+                        this.destroyAllSpawnedObjects();
+                    };
+                    c.element = button;
+                    c.autoSize = true;
+                    c.viewScale = 0.04;
+                }));
+
+        for (let i = 0; i < 10; i++) {
+            this.spawnAtGameObject();
+        }
+    }
+}
+
+export class RandomSpawner5 extends Component {
+    private readonly _range: number = 6;
+    private readonly _spawnedObjects: GameObject[] = [];
+
+    public spawnAtGameObject(): void {
+        const instantiater = this.engine.instantiater;
+        
+        const x = Math.random() * this._range - this._range / 2;
+        const y = Math.random() * this._range - this._range / 2;
+        const position = new Vector3(x, y, 0);
+
+        const spawnedObject = this.gameObject.addChildFromBuilder(
+            instantiater.buildGameObject("spawned-game-object", position)
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const div = document.createElement("div");
+                    div.style.backgroundColor = "black";
+                    div.style.color = "white";
+                    div.style.padding = "10px";
+                    div.textContent = "Spawned At GameObject";
+
+                    c.element = div;
+                    c.autoSize = true;
+                    c.viewScale = 0.02;
+                }));
+
+        this._spawnedObjects.push(spawnedObject);
+    }
+
+    public destroyAllSpawnedObjects(): void {
+        for (const spawnedObject of this._spawnedObjects) {
+            spawnedObject.destroy();
+        }
+    }
+
+    public awake(): void {
+        const textRenderer = this.gameObject.addComponent(CssTextRenderer)!;
+        textRenderer.text = "Spawner";
+        textRenderer.textColor = new Color(1, 0, 0);
+        textRenderer.textAlign = TextAlign.Center;
+        textRenderer.textWidth = 5;
+
+        const removeAllButtonRenderer = new PrefabRef<CssHtmlElementRenderer>();
+        
+        this.engine.scene.addChildFromBuilder(
+            this.engine.instantiater.buildGameObject("remove-all-spawned-objects-button", new Vector3(0, 0, 5))
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const button = document.createElement("button");
+                    button.textContent = "Remove All Spawned Objects";
+                    button.style.backgroundColor = "#990000";
+                    button.style.color = "white";
+                    button.style.padding = "10px";
+                    button.onclick = () => {
+                        this.destroyAllSpawnedObjects();
+                    };
+                    c.element = button;
+                    c.autoSize = true;
+                    c.viewScale = 0.04;
+                })
+                .getComponent(CssHtmlElementRenderer, removeAllButtonRenderer));
+
+        this.engine.scene.addChildFromBuilder(
+            this.engine.instantiater.buildGameObject("destroy-button", new Vector3(0, -2, 5))
+                .withComponent(CssHtmlElementRenderer, c => {
+                    const button = document.createElement("button");
+                    button.textContent = "Destroy Remove All Spawned Objects Button";
+                    button.style.backgroundColor = "#990000";
+                    button.style.color = "white";
+                    button.style.padding = "10px";
+                    button.onclick = () => {
+                        removeAllButtonRenderer.ref!.destroy();
+                    };
+                    c.element = button;
+                    c.autoSize = true;
+                    c.viewScale = 0.04;
+                }));
+
+        for (let i = 0; i < 10; i++) {
+            this.spawnAtGameObject();
+        }
+    }
+}
