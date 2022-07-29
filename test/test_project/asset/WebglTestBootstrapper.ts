8e2e354f49a8f9b9f29075df69b6944fa432cea6
noname <48761044+noname0310@users.noreply.github.com>
2022-07-29 23:45:51 +0900

    test: add OrbitControls Wrapper

--- test/test_project/asset/WebglTestBootstrapper.ts
+++ test/test_project/asset/WebglTestBootstrapper.ts
@@ -1,16 +1,14 @@
 import { Bootstrapper } from "@src/engine/bootstrap/Bootstrapper";
 import { SceneBuilder } from "@src/engine/bootstrap/SceneBuilder";
-import { Component } from "@src/engine/hierarchy_object/Component";
 import { CSS3DObject } from "@src/engine/render/CSS3DRenderer";
 import { Camera, CameraType } from "@src/engine/script/render/Camera";
 import { CssSpriteRenderer } from "@src/engine/script/render/CssSpriteRenderer";
-import { DuckThreeCamera } from "@src/engine/script/render/DuckThreeCamera";
 import { Object3DContainer } from "@src/engine/script/three/Object3DContainer";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { OutlineEffect } from "three/examples/jsm/effects/OutlineEffect";
 import { AmbientLight, BoxGeometry, DirectionalLight, Mesh, MeshBasicMaterial, Vector3, WebGLRenderer } from "three/src/Three";
 
 import { TopDownScenePrefab } from "./prefab/TopDownScenePrefab";
+import { OrbitControls } from "./script/OrbitControls";
 
 export class WebglTestBootstrapper extends Bootstrapper {
     public override run(): SceneBuilder {
@@ -28,41 +26,9 @@ export class WebglTestBootstrapper extends Bootstrapper {
                 .withComponent(Camera, c => {
                     c.cameraType = CameraType.Perspective;
                 })
-                .withComponent(class extends Component {
-                    private _camera: Camera|null = null;
-                    private _orbitControls: OrbitControls|null = null;
-
-                    public awake(): void {
-                        this._camera = this.gameObject.getComponent(Camera);
-                    }
-
-                    public start(): void {
-                        const controls = this._orbitControls = new OrbitControls(
-                            DuckThreeCamera.createInterface(this._camera!),
-                            this.engine.domElement
-                        );
-                        controls.listenToKeyEvents( window ); // optional
-        
-                        controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-                        controls.dampingFactor = 0.05;
-        
-                        controls.screenSpacePanning = true;
-        
-                        controls.minDistance = 20;
-                        controls.maxDistance = 50;
-        
-                        controls.maxPolarAngle = Math.PI / 2;
-                    }
-
-                    public update(): void {
-                        this._orbitControls!.update();
-                    }
-
-                    public onDestroy(): void {
-                        this._orbitControls!.dispose();
-                        this._orbitControls = null;
-                        this._camera = null;
-                    }
+                .withComponent(OrbitControls, c => {
+                    c.target = new Vector3(0, 4, 0);
+                    (globalThis as any).controls = c;
                 }))
                 
             .withChild(instantiater.buildGameObject("ambient-light")
---
+++ test/test_project/asset/script/OrbitControls.ts
@@ -0,0 +1,129 @@
+import { Component } from "@src/engine/hierarchy_object/Component";
+import { ReadonlyVector3 } from "@src/engine/math/ReadonlyVector3";
+import { WritableVector3 } from "@src/engine/math/WritableVector3";
+import { Camera } from "@src/engine/script/render/Camera";
+import { DuckThreeCamera } from "@src/engine/script/render/DuckThreeCamera";
+import { OrbitControls as ThreeOrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Vector3 } from "three/src/Three";
+
+export class OrbitControls extends Component {
+    private _camera: Camera|null = null;
+    private _orbitControls: ThreeOrbitControls|null = null;
+
+    private readonly _target: Vector3 = new Vector3(0, 0, 0);
+    private _minDistance = 20;
+    private _maxDistance = 50;
+    private _maxPolarAngle = Math.PI / 2;
+    private _enableDamping = true;
+    private _dampingFactor = 0.05;
+
+    public awake(): void {
+        this._camera = this.gameObject.getComponent(Camera);
+    }
+
+    public start(): void {
+        const controls = this._orbitControls = new ThreeOrbitControls(
+            DuckThreeCamera.createInterface(this._camera!),
+            this.engine.domElement
+        );
+        controls.listenToKeyEvents(window);
+
+        controls.enableDamping = this._enableDamping;
+        controls.dampingFactor = this._dampingFactor;
+        controls.screenSpacePanning = true;
+        controls.minDistance = this._minDistance;
+        controls.maxDistance = this._maxDistance;
+
+        controls.maxPolarAngle = this._maxPolarAngle;
+
+        controls.target = this._target;
+    }
+
+    public onEnable(): void {
+        if (this._orbitControls) {
+            this._orbitControls.enabled = true;
+        }
+    }
+
+    public onDisable(): void {
+        if (this._orbitControls) {
+            this._orbitControls.enabled = false;
+        }
+    }
+
+    public update(): void {
+        this._orbitControls!.update();
+    }
+
+    public onDestroy(): void {
+        this._orbitControls!.dispose();
+        this._orbitControls = null;
+        this._camera = null;
+    }
+
+    public get target(): ReadonlyVector3 {
+        return this._target;
+    }
+
+    public set target(value: ReadonlyVector3) {
+        (this._target as WritableVector3).copy(value);
+        if (this._orbitControls) {
+            this._orbitControls.target = this._target;
+        }
+    }
+
+    public get minDistance(): number {
+        return this._minDistance;
+    }
+
+    public set minDistance(value: number) {
+        this._minDistance = value;
+        if (this._orbitControls) {
+            this._orbitControls.minDistance = value;
+        }
+    }
+
+    public get maxDistance(): number {
+        return this._maxDistance;
+    }
+
+    public set maxDistance(value: number) {
+        this._maxDistance = value;
+        if (this._orbitControls) {
+            this._orbitControls.maxDistance = value;
+        }
+    }
+
+    public get maxPolarAngle(): number {
+        return this._maxPolarAngle;
+    }
+
+    public set maxPolarAngle(value: number) {
+        this._maxPolarAngle = value;
+        if (this._orbitControls) {
+            this._orbitControls.maxPolarAngle = value;
+        }
+    }
+
+    public get enableDamping(): boolean {
+        return this._enableDamping;
+    }
+
+    public set enableDamping(value: boolean) {
+        this._enableDamping = value;
+        if (this._orbitControls) {
+            this._orbitControls.enableDamping = value;
+        }
+    }
+
+    public get dampingFactor(): number {
+        return this._dampingFactor;
+    }
+
+    public set dampingFactor(value: number) {
+        this._dampingFactor = value;
+        if (this._orbitControls) {
+            this._orbitControls.dampingFactor = value;
+        }
+    }
+}
