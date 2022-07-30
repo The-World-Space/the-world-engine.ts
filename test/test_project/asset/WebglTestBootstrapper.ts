cd5db3617fda3dce07e3a9bb5792a2d538245f0b
noname <48761044+noname0310@users.noreply.github.com>
2022-07-30 11:40:12 +0900

    test: render pass wip

--- test/test_project/asset/WebglTestBootstrapper.ts
+++ test/test_project/asset/WebglTestBootstrapper.ts
@@ -5,19 +5,39 @@ import { Camera, CameraType } from "@src/engine/script/render/Camera";
 import { CssSpriteRenderer } from "@src/engine/script/render/CssSpriteRenderer";
 import { Object3DContainer } from "@src/engine/script/three/Object3DContainer";
 import { OutlineEffect } from "three/examples/jsm/effects/OutlineEffect";
-import { AmbientLight, BoxGeometry, DirectionalLight, Mesh, MeshBasicMaterial, Vector3, WebGLRenderer } from "three/src/Three";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+//import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
+import { AmbientLight, BoxGeometry, DirectionalLight, Mesh, MeshPhongMaterial, PlaneGeometry, Quaternion, Vector2, Vector3, WebGLRenderer } from "three/src/Three";
+import * as THREE from "three/src/Three";
 
 import { TopDownScenePrefab } from "./prefab/TopDownScenePrefab";
 import { OrbitControls } from "./script/OrbitControls";
 
 export class WebglTestBootstrapper extends Bootstrapper {
     public override run(): SceneBuilder {
-        this.setting.render.useCss3DRenderer(true);
-
+        this.setting.render.useCss3DRenderer(false);
         const webGLRenderer = new WebGLRenderer({ antialias: true });
         webGLRenderer.setPixelRatio(window.devicePixelRatio);
-        const effect = new OutlineEffect(webGLRenderer);
-        this.setting.render.webGLRenderer(effect, webGLRenderer.domElement);
+        webGLRenderer.shadowMap.enabled = true;
+        webGLRenderer.toneMapping = THREE.ReinhardToneMapping;
+
+        const outlineEffect = new OutlineEffect(webGLRenderer);
+
+        // todo: support render pass
+        // todo: support post processing
+
+        //const renderScene = new RenderPass( scene, camera );
+
+        const bloomPass = new UnrealBloomPass( new Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+        bloomPass.threshold = 0;
+        bloomPass.strength = 1.5;
+        bloomPass.radius = 0;
+
+        const composer = new EffectComposer(webGLRenderer);
+        //composer.addPass(renderScene);
+        composer.addPass(bloomPass);
+        this.setting.render.webGLRenderer(outlineEffect, webGLRenderer.domElement);
 
         const instantiater = this.instantiater;
 
@@ -27,7 +47,7 @@ export class WebglTestBootstrapper extends Bootstrapper {
                     c.cameraType = CameraType.Perspective;
                 })
                 .withComponent(OrbitControls, c => {
-                    c.target = new Vector3(0, 4, 0);
+                    c.enableDamping = false;
                     (globalThis as any).controls = c;
                 }))
                 
@@ -37,12 +57,14 @@ export class WebglTestBootstrapper extends Bootstrapper {
             .withChild(instantiater.buildGameObject("directional-light", new Vector3(-1, 1, 1).normalize())
                 .withComponent(Object3DContainer, c => c.object3D = new DirectionalLight(0x887766)))
 
-            .withChild(instantiater.buildGameObject("sprite", new Vector3(0, 0, 10))
+            .withChild(instantiater.buildGameObject("sprite", new Vector3(0, 0, 0))
+                .active(true)
                 .withComponent(CssSpriteRenderer, c => {
                     c.pointerEvents = true;
                 }))
 
-            .withChild(instantiater.buildPrefab("top_down_scene", TopDownScenePrefab,  new Vector3(0, -10, 0)).make())
+            .withChild(instantiater.buildPrefab("top_down_scene", TopDownScenePrefab, new Vector3(0, -10, 0)).make()
+                .active(false))
 
             .withChild(instantiater.buildGameObject("csssprite-test")
                 .withComponent(Object3DContainer, c => {
@@ -52,16 +74,29 @@ export class WebglTestBootstrapper extends Bootstrapper {
                     div.textContent = "Hello World";
                     const renderer = new CSS3DObject(div);
                     c.object3D = renderer;
-                    (globalThis as any).sprite = c;
+                    c.enabled = false;
                 }))
 
-            .withChild(instantiater.buildGameObject("test-object", new Vector3(0, 0, -10))
+            .withChild(instantiater.buildGameObject("test-object", new Vector3(0, 0, 0))
                 .withComponent(Object3DContainer, c => {
-                    const geometry = new BoxGeometry(1, 1, 1);
-                    const material = new MeshBasicMaterial({ color: 0x00ff00 });
+                    const geometry = new BoxGeometry(10, 10, 10);
+                    const material = new MeshPhongMaterial({ color: 0x00ff00 });
                     const cube = new Mesh(geometry, material);
+                    cube.castShadow = true;
+                    cube.receiveShadow = true;
                     c.object3D = cube;
                     (globalThis as any).cube = c;
+                }))
+            
+            .withChild(instantiater.buildGameObject("plane",
+                new Vector3(0, -5, 0),
+                new Quaternion().setFromAxisAngle(new Vector3(1, 0, 0), -Math.PI / 2))
+                .withComponent(Object3DContainer, c => {
+                    const geometry = new PlaneGeometry(100, 100);
+                    const material = new MeshPhongMaterial({ color: 0xffffff });
+                    const plane = new Mesh(geometry, material);
+                    plane.receiveShadow = true;
+                    c.object3D = plane;
                 }))
         ;
     }
